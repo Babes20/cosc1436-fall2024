e3f76096e60b313919d2649783c66a5d008f6171
frehiwot tadesse <tadessefrehiwot@yahoo.com>
2024-10-17 18:34:52 -0500

    program info

---
+++ labs/FallingDistance/FallingDistance.cpp
@@ -0,0 +1,20 @@
+// Student Name : Frehiwot Tadesse
+// Tarrant County College - COSC 1436
+// Semester / Course: COSC 1436 Fall 2024
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+
+    //Include the std namespace so we don't have to to prefix it on everything
+using namespace std;
+
+int main() {
+
+    // Function to display program information
+    cout << "FallingDistance" << endl;
+    cout << "Frehiwot Tadesse" << endl;
+    cout << "COSC 1436, Fall 2024" << endl;
+    cout << endl;
+
+// Display program information
