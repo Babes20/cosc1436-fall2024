c488e5cd02807d8f3fb3307ac3dd485269eec94e
frehiwot tadesse <tadessefrehiwot@yahoo.com>
2024-10-23 14:08:08 -0500

    displayproginfo

--- labs/FallingDistance/FallingDistance.cpp
+++ labs/FallingDistance/FallingDistance.cpp
@@ -9,12 +9,34 @@
     //Include the std namespace so we don't have to to prefix it on everything
 using namespace std;
 
+
+double Gravity = 9.8;
+double Meter_ or_ Feet = 3.28084;
+
+void displayProgramInfo();
+int promptForTime();
+char promptForUnit();
+
+
+
 int main() {
 
-    // Function to display program information
+// Display program information
+    displayProgramInfo();
+
+    // prompt the user for input
+
+    int time = promptForTime();
+    char unit = promptForUnit();
+
+
+    return 0;
+
+}
+void displayProgramInfo() {
+
     cout << "FallingDistance" << endl;
     cout << "Frehiwot Tadesse" << endl;
     cout << "COSC 1436, Fall 2024" << endl;
-    cout << endl;
-
-// Display program information
+    cout << "=====================" << endl;
+}
