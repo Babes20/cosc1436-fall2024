c5fa12d5b9167d279e77b2975410c9ab7d759ec6
frehiwot tadesse <tadessefrehiwot@yahoo.com>
2024-11-10 17:19:34 -0600

    menu and user choice

---
+++ labs/Lab4/Lab4.cpp
@@ -0,0 +1,43 @@
+// Student Name : Frehiwot Tadesse
+// Tarrant County College - COSC 1436
+// Semester / Course: COSC 1436 Fall 2024
+#include <iostream>
+#include <iomanip>
+
+
+
+    //Include the std namespace so we don't have to to prefix it on everything
+using namespace std;
+
+void displayProgramInfo() {
+
+    cout << "lab 4" << endl;
+    cout << "Frehiwot Tadesse" << endl;
+    cout << "COSC 1436, Fall 2024" << endl;
+}
+
+// function for values and populate array
+int Array(int numbers[], int maxSize) {
+
+    int count = 0;
+    int value;
+
+    while (count < maxsize) {
+        cout << "erroe: omly positive values are allowed " << endl;
+    }
+
+    number[count++] = value;
+
+    }
+    return count;
+}
+
+// display menu and user choice
+char displaymenu() {
+    cout << "Main menu << endl";
+    cout << "A) add values << endl";
+    cout << "L) Largest value endl";
+    cout << "S) smallest value << endl";
+    cout << "M) mean << endl";
+    cout <<
+    cout <<
