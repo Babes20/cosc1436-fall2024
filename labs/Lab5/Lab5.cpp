1ce264981a4780ac81e33a2366e4aaf1f0dee2a7
frehiwot tadesse <tadessefrehiwot@yahoo.com>
2024-12-03 20:12:31 -0600

    linked list

--- labs/Lab5/Lab5.cpp
+++ labs/Lab5/Lab5.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-//
+// define a Node
 
 struct Node {
     int value = 0;
@@ -14,7 +14,7 @@ struct LinkedList {
     Node* head = nullptr;
 };
 
-// Function prototypes
+//Prototypes
 void displayProgramInfo();
 void displayMainMenu();
 char getMenuChoice();
@@ -23,40 +23,46 @@ void listValues(const LinkedList& list);
 void deleteValue(LinkedList& list);
 void clearList(LinkedList& list);
 
+
 int main() {
     displayProgramInfo();
-    LinkedList list;
+
+    LinkedList list; 
     char choice;
 
     do {
         displayMainMenu();
         choice = getMenuChoice();
 
-        switch ((choice)) {
+        switch (choice) {
+            case 'A':
             case 'a':
                 addValue(list);
                 break;
+            case 'L':
             case 'l':
                 listValues(list);
                 break;
+            case 'D':
             case 'd':
                 deleteValue(list);
                 break;
+            case 'C':
             case 'c':
                 clearList(list);
                 break;
+            case 'Q':
             case 'q':
                 cout << "Exiting program. Goodbye!" << endl;
                 break;
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
-    } while (choice) != 'q';
-    clearList(list);
+    } while (choice != 'Q' && choice != 'q');
+
+    clearList(list); 
     return 0;
 }
-
-
 // program information
 void displayProgramInfo() {
     cout << "lab 4" << endl;
@@ -65,9 +71,8 @@ void displayProgramInfo() {
     cout << "--------------------------------" << endl;
 }
 
-// display the main menu
 void displayMainMenu() {
-    cout << "Main Menu" << endl;
+    cout << "\nMain Menu" << endl;
     cout << "--------------------" << endl;
     cout << "L) List" << endl;
     cout << "A) Add" << endl;
@@ -76,99 +81,103 @@ void displayMainMenu() {
     cout << "Q) Quit" << endl;
 }
 
+// Function to get a valid menu choice from the user
 char getMenuChoice() {
     char choice;
+    cout << "? ";
+    cin >> choice;
     return choice;
 }
+
+// Function to add a value to the linked list
 void addValue(LinkedList& list) {
     int value;
     cout << "Enter the integral value to add: ";
     while (!(cin >> value)) {
         cout << "Invalid input. Please enter an integer: ";
         cin.clear();
-
-        //
-  Node* newNode = new Node {value, nullptr};
-        if (!list.head) {
-            list.head = newNode;
-        } else {
-            Node* current = list.head;
-            while (current->next) {
-                current = current->next;
-            }
-            current->next = newNode;
-        }
-        cout << "Value added to the list." << endl;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    //list all values in the linked list
-    void listValues(const LinkedList & list) {
-        if (!list.head) {
-            cout << "The list is empty." << endl;
-            return;
-        }
+    Node* newNode = new Node {value, nullptr};
+    if (!list.head) {
+        list.head = newNode;
+    } else {
         Node* current = list.head;
-        cout << "List contents: ";
-        while (current) {
-            cout << current->value << " ";
+        while (current->next) {
             current = current->next;
         }
-        cout << endl;
+        current->next = newNode;
+    }
+    cout << "Value added to the list." << endl;
+}
+
+//  list all values in the linked list
+void listValues(const LinkedList& list) {
+    if (!list.head) {
+        cout << "The list is empty." << endl;
+        return;
+    }
+    Node* current = list.head;
+    cout << "List contents: ";
+    while (current) {
+        cout << current->value << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+//  delete a value from the linked list
+void deleteValue(LinkedList& list) {
+    if (!list.head) {
+        cout << "The list is empty. Nothing to delete." << endl;
+        return;
     }
 
-    //delete a value from the linked list
+    int value;
+    cout << "Enter the integral value to remove: ";
+    while (!(cin >> value)) {
+        cout << "Invalid input. Please enter an integer: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
 
-    void deleteValue(LinkedList& list) {
-        if (!list.head) {
-            cout << "The list is empty. Nothing to delete." << endl;
-            return;
-        }
+    Node* current = list.head;
+    Node* previous = nullptr;
 
-        int value;
-        cout << "Enter the integral value to remove: ";
-        while (!(cin >> value)) {
-            cout << "Invalid input. Please enter an integer: ";
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
-
-        Node* current = list.head;
-        Node* previous = nullptr;
-
-        while (current) {
-            if (current->value == value) {
-                if (previous) {
-                    previous->next = current->next;
-                } else {
-                    list.head = current->next;
-                }
-                delete current;
-                cout << "Value removed from the list." << endl;
-                return;
+    while (current) {
+        if (current->value == value) {
+            if (previous) {
+                previous->next = current->next;
+            } else {
+                list.head = current->next;
             }
-            previous = current;
-            current = current->next;
-        }
-        cout << "Value not found in the list." << endl;
-    }
-
-    //clear all values from the linked list
-
-    void clearList(LinkedList& list) {
-        char confirm;
-        cout << "Are you sure you want to clear the list? (Y/N): ";
-        cin >> confirm;
-        if (tolower(confirm) != 'y') {
-            cout << "List not cleared." << endl;
-            return;
-        }
-
-        Node* current = list.head;
-        while (current) {
-            Node* next = current->next;
             delete current;
-            current = next;
+            cout << "Value removed from the list." << endl;
+            return;
         }
-        list.head = nullptr;
-        cout << "List cleared." << endl;
+        previous = current;
+        current = current->next;
     }
+    cout << "Value not found in the list." << endl;
+}
+
+// clear all values from the linked list
+void clearList(LinkedList& list) {
+    char confirm;
+    cout << "Are you sure you want to clear the list? (Y/N): ";
+    cin >> confirm;
+    if (confirm != 'Y' && confirm != 'y') {
+        cout << "List not cleared." << endl;
+        return;
+    }
+
+    Node* current = list.head;
+    while (current) {
+        Node* next = current->next;
+        delete current;
+        current = next;
+    }
+    list.head = nullptr;
+    cout << "List cleared." << endl;
+}
